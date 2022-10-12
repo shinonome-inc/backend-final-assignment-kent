7f8bfc202119f5f1ba75c96d7ee866b1c82b2c6e
kent8192 <k.kent8192@gmail.com>
2022-10-12 21:37:38 +0900

    Comment out 2 models

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# class User(AbstractUser):
-#     pass
+class User(AbstractUser):
+    pass
 
-# class FriendShip(models.Model):
-#     pass
+
+class FriendShip(models.Model):
+    pass
