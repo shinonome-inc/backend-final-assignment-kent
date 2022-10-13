aca6ff8d7c01433484f53392fb351f05b7164a80
kent8192 <k.kent8192@gmail.com>
2022-10-13 21:50:18 +0900

    add SignUp URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
-# from . import views
+
+from . import views
 
 app_name = "accounts"
 urlpatterns = [
     # path('', views.WelcomeView.as_view(), name='welcome'),
-    # path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
     # path('home/', views.HomeView.as_view(), name='home'),
     path("", include("django.contrib.auth.urls")),
     # path('profile/', views.UserProfileView.as_view(), name='user_profile'),
