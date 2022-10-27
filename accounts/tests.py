e1db29572e54d805be6a2d08b62454f4e4db2e80
kent8192 <kento.kiku@outlook.jp>
2022-10-27 19:04:18 +0900

    implement required functions:testing signup,home

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,26 +1,28 @@
+import json
+
+import colorama
+from colorama import Fore
+from django.core import serializers
 from django.test import TestCase
 from django.urls import reverse
 
 from accounts.forms import UserCreateForm
 
+from .models import User
 
-def print_red(code):
-    print("\n" + "\033[31m" + f"{code}" + "\033[0m")
+
+def print_colored_text(code, *styles):
+    colorama.init()
+    text_to_print = ""
+    for style in styles:
+        text_to_print += style
+    text_to_print += str(code)
+    print(f"{text_to_print}" + Fore.RESET)
 
 
 class TestSignUpView(TestCase):
-    def setUp(self):
-        self.url = reverse("accounts:signup")
-
-    def runTest():
-        test = TestSignUpView()
-        # test.test_success_get()
-        test.test_success_post()
-        # test.test_failure_post_with_empty_form()
-        # test.test_failure_post_with_empty_username()
-
     def test_success_get(self):
-        response = self.client.get(reverse("accounts:home"))
+        response = self.client.get(reverse("accounts:signup"))
         self.assertEqual(response.status_code, 200)
 
     def test_success_post(self):
@@ -31,146 +33,182 @@ class TestSignUpView(TestCase):
             "password2": "passcode0000",
         }
         response = self.client.post(reverse("accounts:signup"), data)
-        # リダイレクトされるため200は返ってこないが、302が返ってくる
-        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("accounts:home"), 302, 200)
+        added_record = User.objects.filter(username="test")
+        self.assertTrue(added_record.exists())
+        added_record_json = serializers.serialize("json", added_record)
+        json_data = json.loads(added_record_json)
+        self.assertEquals(json_data[0]["fields"]["username"], "test")
+        self.assertEquals(json_data[0]["fields"]["email"], "hoge@email.com")
 
     def test_failure_post_with_empty_form(self):
-        data_empty_form = {
+        data = {
             "username": "",
             "email": "",
             "password1": "",
             "password2": "",
         }
-        form = UserCreateForm(data_empty_form)
+        form = UserCreateForm(data)
         form.is_valid()
-        print_red(f"!!! test_failure_post_with_empty_form !!! : {form.errors.as_data}")
+        expected_error_mes = "{'username': [ValidationError(['このフィールドは必須です。'])], 'email': [ValidationError(['このフィールドは必須です。'])], 'password1': [ValidationError(['このフィールドは必須です。'])], 'password2': [ValidationError(['このフィールドは必須です。'])]}"
+        self.assertEqual(expected_error_mes, str(form.errors.as_data()))
+        response = self.client.post(reverse("accounts:signup"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 0)
 
     def test_failure_post_with_empty_username(self):
-        data_empty_name = {
+        data = {
             "username": "",
             "email": "hoge@email.com",
             "password1": "pass0000",
             "password2": "pass0000",
         }
-        form = UserCreateForm(data_empty_name)
+        form = UserCreateForm(data)
         form.is_valid()
-        print_red(
-            f"!!! test_failure_post_with_empty_username !!!: {form.errors.as_data}"
-        )
+        expected_error_mes = "{'username': [ValidationError(['このフィールドは必須です。'])]}"
+        self.assertEqual(expected_error_mes, str(form.errors.as_data()))
+        response = self.client.post(reverse("accounts:signup"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 0)
 
     def test_failure_post_with_empty_email(self):
-        data_empty_email = {
+        data = {
             "username": "test",
             "email": "",
             "password1": "pass0000",
             "password2": "pass0000",
         }
-        form = UserCreateForm(data_empty_email)
+        form = UserCreateForm(data)
         form.is_valid()
-        print_red(f"!!! test_failure_post_with_empty_email !!! : {form.errors.as_data}")
+        expected_error_mes = "{'email': [ValidationError(['このフィールドは必須です。'])]}"
+        self.assertEqual(expected_error_mes, str(form.errors.as_data()))
+        response = self.client.post(reverse("accounts:signup"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 0)
 
     def test_failure_post_with_empty_password(self):
-        data_empty_passwword = {
+        data = {
             "username": "test",
             "email": "hoge@email.com",
             "password1": "",
             "password2": "",
         }
-        form = UserCreateForm(data_empty_passwword)
+        form = UserCreateForm(data)
         form.is_valid()
-        print_red(
-            f"!!! test_failure_post_with_empty_password !!! : {form.errors.as_data}"
-        )
+        expected_error_mes = "{'password1': [ValidationError(['このフィールドは必須です。'])], 'password2': [ValidationError(['このフィールドは必須です。'])]}"
+        self.assertEqual(expected_error_mes, str(form.errors.as_data()))
+        response = self.client.post(reverse("accounts:signup"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 0)
 
     def test_failure_post_with_duplicated_user(self):
-        duplicated_user_data = {
+        data1 = {
             "username": "test",
             "email": "fuga@email.com",
             "password1": "passcode0000",
             "password2": "passcode0000",
         }
-        self.client.post(reverse("accounts:signup"), duplicated_user_data)
-        duplicated_user_data = {
+        self.client.post(reverse("accounts:signup"), data1)
+        data2 = {
             "username": "test",
             "email": "fuga@email.com",
             "password1": "passcode0000",
             "password2": "passcode0000",
         }
-        form = UserCreateForm(duplicated_user_data)
+        form = UserCreateForm(data2)
         form.is_valid()
-        print_red(
-            f"!!! test_failure_post_with_duplicated_user !!! : {form.errors.as_data}"
-        )
+        expected_error_mes = "{'username': [ValidationError(['同じユーザー名が既に登録済みです。'])]}"
+        self.assertEqual(expected_error_mes, str(form.errors.as_data()))
+        response = self.client.post(reverse("accounts:signup"), data2)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 1)
 
     def test_failure_post_with_invalid_email(self):
-        data_invalid_email = {
+        data = {
             "username": "testpscd",
             "email": "fuga.email.com",
             "password1": "test0000",
             "password2": "test0000",
         }
-        form = UserCreateForm(data_invalid_email)
+        form = UserCreateForm(data)
         form.is_valid()
-        print_red(
-            f"!!! test_failure_post_with_invalid_email !!! : {form.errors.as_data}"
-        )
+        expected_error_mes = "{'email': [ValidationError(['有効なメールアドレスを入力してください。'])]}"
+        self.assertEqual(expected_error_mes, str(form.errors.as_data()))
+        response = self.client.post(reverse("accounts:signup"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 0)
 
     def test_failure_post_with_too_short_password(self):
-        data_too_short_password = {
+        data = {
             "username": "test",
             "email": "fuga@email.com",
             "password1": "passcd",
             "password2": "passcd",
         }
-        form = UserCreateForm(data_too_short_password)
+        form = UserCreateForm(data)
         form.is_valid()
-        print_red(
-            f"!!! test_failure_post_with_too_short_password !!! : {form.errors.as_data}"
+        expected_error_mes = (
+            "{'password2': [ValidationError(['このパスワードは短すぎます。最低 8 文字以上必要です。'])]}"
         )
+        self.assertEqual(expected_error_mes, str(form.errors.as_data()))
+        response = self.client.post(reverse("accounts:signup"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 0)
 
     def test_failure_post_with_password_similar_to_username(self):
-        data_similar_to_username = {
+        data = {
             "username": "testpscd",
             "email": "fuga@email.com",
             "password1": "testpscd",
             "password2": "testpscd",
         }
-        form = UserCreateForm(data_similar_to_username)
+        form = UserCreateForm(data)
         form.is_valid()
-        print_red(
-            f"!!! test_failure_post_with_password_similar_to_username !!! : {form.errors.as_data}"
+        expected_error_mes = (
+            "{'password2': [ValidationError(['このパスワードは ユーザー名 と似すぎています。'])]}"
         )
+        self.assertEqual(expected_error_mes, str(form.errors.as_data()))
+        response = self.client.post(reverse("accounts:signup"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 0)
 
     def test_failure_post_with_only_numbers_password(self):
-        data_only_numbers_password = {
+        data = {
             "username": "testpscd",
             "email": "fuga@email.com",
             "password1": "20040326",
             "password2": "20040326",
         }
-        form = UserCreateForm(data_only_numbers_password)
+        form = UserCreateForm(data)
         form.is_valid()
-        print_red(
-            f"!!! test_failure_post_with_only_numbers_password !!! : {form.errors.as_data}"
+        expected_error_mes = (
+            "{'password2': [ValidationError(['このパスワードは数字しか使われていません。'])]}"
         )
+        self.assertEqual(expected_error_mes, str(form.errors.as_data()))
+        response = self.client.post(reverse("accounts:signup"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 0)
 
     def test_failure_post_with_mismatch_password(self):
-        data_only_numbers_password = {
+        data = {
             "username": "testpscd",
             "email": "fuga@email.com",
             "password1": "testpasscd",
             "password2": "testpassdc",
         }
-        form = UserCreateForm(data_only_numbers_password)
+        form = UserCreateForm(data)
         form.is_valid()
-        print_red(
-            f"!!! test_failure_post_with_mismatch_password !!! : {form.errors.as_data}"
-        )
+        expected_error_mes = "{'password2': [ValidationError(['確認用パスワードが一致しません。'])]}"
+        self.assertEqual(expected_error_mes, str(form.errors.as_data()))
+        response = self.client.post(reverse("accounts:signup"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 0)
 
 
 class TestHomeView(TestCase):
     def test_success_get(self):
-        pass
+        response = self.client.get(reverse("accounts:home"))
+        self.assertEqual(response.status_code, 200)
 
 
 class TestLoginView(TestCase):
