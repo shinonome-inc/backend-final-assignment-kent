dbe49a0362f8162ebee14d8f10272d4226f28dc9
kent8192 <kento.kiku@outlook.jp>
2022-10-27 19:04:30 +0900

    change urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,12 @@
-from django.urls import include, path
+from django.urls import path
 
 from . import views
 
 app_name = "accounts"
 urlpatterns = [
     # path('', views.WelcomeView.as_view(), name='welcome'),
-    path("signup/", views.SignUpView.as_view(), name="signup"),
-    path("home/", views.HomeView.as_view(), name="home"),
-    path("", include("django.contrib.auth.urls")),
+    path("signup", views.SignUpView.as_view(), name="signup"),
+    path("home", views.HomeView.as_view(), name="home"),
     # path('profile/', views.UserProfileView.as_view(), name='user_profile'),
     # path('profile/edit/', views.UserProfileEditView.as_view(), name='user_profile_edit'),
     # path('<str:username>/following_list/', views.FollowingListView.as_view(), name='following_list'),
