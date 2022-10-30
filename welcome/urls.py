e40cebbd0c9fe2bab0df19e3e8721f154111b5a0
kent8192 <kento.kiku@outlook.jp>
2022-10-30 16:55:45 +0900

    format the file: urls.py

--- welcome/urls.py
+++ welcome/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from . import views
-
-app_name = 'welcome'
+app_name = "welcome"
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='welcome/index.html')),
+    path("", TemplateView.as_view(template_name="welcome/index.html"), name="home"),
 ]
