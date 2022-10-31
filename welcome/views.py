6e1743254d08c7cfedb145e3580296632347072a
kent8192 <kento.kiku@outlook.jp>
2022-11-01 08:18:37 +0900

    add the view: WelcomeView

--- welcome/views.py
+++ welcome/views.py
@@ -1,3 +1,32 @@
+from colorama import Fore
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+from django.views.generic import View
+
+from accounts.tests import print_colored_text
+
+
+class WelcomeView(View):
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        signin_url = reverse("accounts:signin")
+        signout_url = reverse("accounts:signout")
+        print_colored_text("signin_url", Fore.BLUE)
+        print_colored_text(
+            "------------------------------------------------------", Fore.BLUE
+        )
+        context = {"user": user, "signin_url": signin_url, "signout_url": signout_url}
+        return render(request, "index.html", context)
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        signin_url = reverse("accounts:signin")
+        signout_url = reverse("accounts:signout")
+        print_colored_text("signin_url", Fore.BLUE)
+        print_colored_text(
+            "------------------------------------------------------", Fore.BLUE
+        )
+        context = {"user": user, "signin_url": signin_url, "signout_url": signout_url}
+        return render(request, "index.html", context)
