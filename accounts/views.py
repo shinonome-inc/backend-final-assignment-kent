3cac6855b4d87d6a092db66a50a732d132e5c975
kent8192 <kento.kiku@outlook.jp>
2022-11-01 08:17:39 +0900

    add the view: SignInView

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views.generic import View
 
-from .forms import SignInForm, UserCreateForm
+from .forms import UserCreateForm, UserSignInForm
 
 
 class SignUpView(View):
@@ -37,9 +37,28 @@ class SignUpView(View):
         )
 
 
-class SignInView(LoginView):
-    template_name = "signin.html"
-    form_class = SignInForm
+class SignInView(View):
+    def post(self, request, *args, **kwargs):
+        form = UserSignInForm(data=request.POST)
+        if form.is_valid():
+            # フォームから'username'を読み取る
+            username = form.cleaned_data.get("username")
+            # フォームから'password'を読み取る
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                # 認証処理
+                login(request, user)
+                return redirect(reverse("welcome:home"))
+            else:
+                return render(request, "signin.html", {"context": "ログインに失敗しました"})
+        else:
+            return render(request, "signin.html", {"context": "formが無効です"})
+
+    def get(self, request, *args, **kwargs):
+        template_name = "signin.html"
+        form = UserSignInForm(request.POST)
+        return render(request, template_name, {"form": form})
 
 
 class SignOutView(View):
