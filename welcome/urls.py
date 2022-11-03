b03ae1b659bf33abcaa264f20f65ebb50a4c890e
kent8192 <kento.kiku@outlook.jp>
2022-11-03 17:55:54 +0900

    delete the invalid link

--- welcome/urls.py
+++ welcome/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 
 app_name = "welcome"
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="welcome/index.html"), name="home"),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
 ]
