dc363046ed6e2bc044bf10d079f8fd51ad6974aa
kent8192 <kento.kiku@outlook.jp>
2022-11-03 20:56:21 +0900

    limit to work only in debug mode

--- mysite/useful_funcs.py
+++ mysite/useful_funcs.py
@@ -1,11 +1,13 @@
 import colorama
 from colorama import Fore
+from django.conf import settings
 
 
 def print_colored_text(code, *styles):
-    colorama.init()
-    text_to_print = ""
-    for style in styles:
-        text_to_print += style
-    text_to_print += str(code)
-    print(f"{text_to_print}" + Fore.RESET)
+    if settings.DEBUG:
+        colorama.init()
+        text_to_print = ""
+        for style in styles:
+            text_to_print += style
+        text_to_print += str(code)
+        print(f"{text_to_print}" + Fore.RESET)
