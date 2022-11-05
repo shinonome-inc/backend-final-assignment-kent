e70d90f515f308f2363a77bb0f5ac7db4ad4b606
kent8192 <kento.kiku@outlook.jp>
2022-11-05 22:53:55 +0900

    add 2 views

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.generic import View
+
 
 # Create your views here.
+class TweetCreateView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("GET request!")
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse("POST request!")
+
+
+class TweetDetailView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("GET request!")
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse("POST request!")
