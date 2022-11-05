1bc906c355556f33bbf971255786fc4929d97bec
kent8192 <kento.kiku@outlook.jp>
2022-11-05 22:54:10 +0900

    add model contents

--- tweets/models.py
+++ tweets/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+from accounts.models import User
+
 
 class Tweet(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
