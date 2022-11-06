108697642a6b19178ae22d68ba9116d65f607035
kent8192 <kento.kiku@outlook.jp>
2022-11-06 21:42:20 +0900

    add contents of TweetCreateView.get()

--- tweets/views.py
+++ tweets/views.py
@@ -5,7 +5,7 @@ from django.views.generic import View
 # Create your views here.
 class TweetCreateView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("GET request!")
+        return render(request, "tweets/create_view.html", {})
 
     def post(self, request, *args, **kwargs):
         return HttpResponse("POST request!")
