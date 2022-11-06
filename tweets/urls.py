edaa4ee1d5bc7b0a7b2540f76ba08a06e0f174ef
kent8192 <kento.kiku@outlook.jp>
2022-11-06 21:41:36 +0900

    add 2 urls

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,13 +1,11 @@
-# trunk-ignore(flake8/F401)
 from django.urls import path
 
-# trunk-ignore(flake8/F401)
 from . import views
 
 app_name = "tweets"
 urlpatterns = [
-    # path('create/', views.TweetCreateView.as_view(), name='create'),
-    # path('<int:pk>/', views.TweetDetailView.as_view(), name='detail'),
+    path("create/", views.TweetCreateView.as_view(), name="create"),
+    path("<int:pk>/", views.TweetDetailView.as_view(), name="detail"),
     # path('<int:pk>/delete/', views.TweetDeleteView.as_view(), name='delete'),
     # path('<int:pk>/like/', views.LikeView, name='like'),
     # path('<int:pk>/unlike/', views.UnlikeView, name='unlike'),
