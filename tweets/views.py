48bbc41f0d34a558fd4c582c7856a909e56ebf26
kent8192 <kento.kiku@outlook.jp>
2022-11-07 15:51:08 +0900

    impl 2 views

--- tweets/views.py
+++ tweets/views.py
@@ -1,27 +1,25 @@
-from django.shortcuts import HttpResponse, render, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import View
 
-from tweets.forms import TweetForm
+from tweets.forms import TweetCreateForm
 from tweets.models import Tweet
 
 
 class TweetCreateView(View):
     def get(self, request, *args, **kwargs):
-        form = TweetForm(request.POST)
-        return render(request, "tweets/tweets_create.html", {"form": form})
+        form = TweetCreateForm(request.POST)
+        return render(request, "tweets_create.html", {"form": form})
 
     def post(self, request, *args, **kwargs):
-        form = TweetForm(data=request.POST)
+        form = TweetCreateForm(data=request.POST)
         if form.is_valid():
             content = form.cleaned_data.get("content")
             Tweet.objects.create(user=request.user, content=content)
             return redirect("welcome:home")
-        return render(request, "tweets/tweets_create.html", {"form": form})
+        return render(request, "tweets_create.html", {"form": form})
 
 
 class TweetDetailView(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("GET request!")
-
-    def post(self, request, *args, **kwargs):
-        return HttpResponse("POST request!")
+    def get(self, request, pk, *args, **kwargs):
+        tweet = get_object_or_404(Tweet, pk=pk)
+        return render(request, "tweets_detail.html", {"tweet": tweet})
