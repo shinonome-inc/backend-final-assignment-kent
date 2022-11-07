f954049fbbbb9742ca832aebb3946a591eb774f3
kent8192 <kento.kiku@outlook.jp>
2022-11-07 12:12:13 +0900

    add contents to TweetCreateView(uncompleted)

--- tweets/views.py
+++ tweets/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, redirect
 from django.views.generic import View
 
+from tweets.forms import TweetForm
+from tweets.models import Tweet
 
-# Create your views here.
+
 class TweetCreateView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "tweets/create_view.html", {})
+        form = TweetForm(request.POST)
+        return render(request, "tweets/tweets_create.html", {"form": form})
 
     def post(self, request, *args, **kwargs):
-        return HttpResponse("POST request!")
+        form = TweetForm(data=request.POST)
+        if form.is_valid():
+            content = form.cleaned_data.get("content")
+            Tweet.objects.create(user=request.user, content=content)
+            return redirect("welcome:home")
+        return render(request, "tweets/tweets_create.html", {"form": form})
 
 
 class TweetDetailView(View):
