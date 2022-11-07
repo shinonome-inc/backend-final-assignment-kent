be90a5a524fda9c89e829dd7ba39633a7eb7373b
kent8192 <kento.kiku@outlook.jp>
2022-11-07 15:53:51 +0900

    add functions to WelcomeView

--- welcome/views.py
+++ welcome/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import View
 
+from tweets.models import Tweet
+
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         user = self.request.user
-        context = {"user": user}
+        tweets = Tweet.objects.filter(user=user).all()
+        context = {"user": user, "tweets": tweets}
         return render(request, "index.html", context)
