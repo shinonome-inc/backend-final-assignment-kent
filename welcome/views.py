97d367838163d5d67933401bd88ad25c71cd0537
kent8192 <kento.kiku@outlook.jp>
2022-11-08 07:15:47 +0900

    fix None bug

--- welcome/views.py
+++ welcome/views.py
@@ -8,7 +8,7 @@ from tweets.models import Tweet
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
-        user = self.request.user
+        user = self.request.user if self.request.user.is_authenticated else None
         tweets = Tweet.objects.filter(user=user).all()
         context = {"user": user, "tweets": tweets}
         return render(request, "index.html", context)
