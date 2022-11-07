f7a84d91740527f6a5744e2a5b1f7ff738d0abda
kent8192 <kento.kiku@outlook.jp>
2022-11-07 15:51:31 +0900

    add __str__() and Meta

--- tweets/models.py
+++ tweets/models.py
@@ -8,3 +8,9 @@ class Tweet(models.Model):
     content = models.CharField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.content} ::: id is {str(Tweet.pk)}"
+
+    class Meta:
+        verbose_name_plural = "ツイート"
