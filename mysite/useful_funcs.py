ab1341b1b988fc59b9d8de679269ea247d8f2758
kent8192 <kento.kiku@outlook.jp>
2022-11-08 13:03:34 +0900

    delete branch by settings.DEBUG

--- mysite/useful_funcs.py
+++ mysite/useful_funcs.py
@@ -1,13 +1,11 @@
 import colorama
-from colorama import Fore
-from django.conf import settings
+from colorama import Back, Fore, Style
 
 
 def print_colored_text(code, *styles):
-    if settings.DEBUG:
-        colorama.init()
-        text_to_print = ""
-        for style in styles:
-            text_to_print += style
-        text_to_print += str(code)
-        print(f"{text_to_print}" + Fore.RESET)
+    colorama.init()
+    text_to_print = ""
+    for style in styles:
+        text_to_print += style
+    text_to_print += str(code)
+    print(f"{text_to_print}" + Fore.RESET, Back.RESET, Style.RESET_ALL)
