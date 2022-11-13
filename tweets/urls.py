a06b1cf7e113c7ab13d16bca04f26aceff1def51
kent8192 <kento.kiku@outlook.jp>
2022-11-13 18:18:05 +0900

    fix conflicts of urls

--- tweets/urls.py
+++ tweets/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "tweets"
 urlpatterns = [
     path("create/", views.TweetCreateView.as_view(), name="create"),
-    path("<int:pk>/", views.TweetDeleteView.as_view(), name="detail"),
+    path("<int:pk>/", views.TweetDetailView.as_view(), name="detail"),
     path("<int:pk>/delete/", views.TweetDeleteView.as_view(), name="delete"),
     # path('<int:pk>/like/', views.LikeView, name='like'),
     # path('<int:pk>/unlike/', views.UnlikeView, name='unlike'),
