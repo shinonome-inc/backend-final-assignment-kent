ed7f20885c15e9924fba7f5fbbc41699bad177d6
kent8192 <kento.kiku@outlook.jp>
2022-11-28 17:46:07 +0900

    change libraries of Path

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -58,7 +57,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, "templates/"),
+            BASE_DIR / "templates/",
         ],
         "APP_DIRS": True,
         "OPTIONS": {
