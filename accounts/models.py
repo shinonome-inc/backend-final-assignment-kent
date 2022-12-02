155328e2f0d00c224418f1d022f8afd8540317f8
kent8192 <kento.kiku@outlook.jp>
2022-12-02 19:18:52 +0900

    add FriendShip model

--- accounts/models.py
+++ accounts/models.py
@@ -7,4 +7,14 @@ class User(AbstractUser):
 
 
 class FriendShip(models.Model):
-    pass
+    follower = models.ForeignKey(
+        User, related_name="follower", on_delete=models.CASCADE
+    )
+    following = models.ForeignKey(
+        User, related_name="following", on_delete=models.CASCADE
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.follower.username} : {self.following.username}"
