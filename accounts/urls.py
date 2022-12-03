1ca2a9832c43d2dfe6c75517aeb200d94b245397
kent8192 <kento.kiku@outlook.jp>
2022-12-03 10:58:19 +0900

    add urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,11 +6,23 @@ app_name = "accounts"
 urlpatterns = [
     path("signup", views.SignUpView.as_view(), name="signup"),
     path("signin", views.SignInView.as_view(), name="signin"),
-    path("signout", views.SignOutView.as_view(), name="signout")
-    # path('profile/', views.UserProfileView.as_view(), name='user_profile'),
-    # path('profile/edit/', views.UserProfileEditView.as_view(), name='user_profile_edit'),
-    # path('<str:username>/following_list/', views.FollowingListView.as_view(), name='following_list'),
-    # path('<str:username>/follower_list/', views.FollowerListView.as_view(), name='follower_list'),
-    # path('<str:username>/follow/', views.FollowView.as_view(), name='follow'),
-    # path('<str:username>/unfollow/', views.UnFollowView, name='unfollow'),
+    path("signout", views.SignOutView.as_view(), name="signout"),
+    path("<str:username>/profile/", views.UserProfileView.as_view(), name="profile"),
+    path(
+        "<str:username>/profile/edit/",
+        views.UserProfileEditView.as_view(),
+        name="profile_edit",
+    ),
+    # path(
+    #    "<str:username>/following_list/",
+    #    views.FollowingListView.as_view(),
+    #    name="following_list",
+    # ),
+    # path(
+    #    "<str:username>/follower_list/",
+    #    views.FollowerListView.as_view(),
+    #    name="follower_list",
+    # ),
+    path("<str:username>/follow/", views.FollowView.as_view(), name="follow"),
+    path("<str:username>/unfollow/", views.UnFollowView.as_view(), name="unfollow"),
 ]
