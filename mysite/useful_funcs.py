97e2cc875fea81d868e1cf7574dcdcb231b0a109
kent8192 <kento.kiku@outlook.jp>
2022-12-18 11:39:37 +0900

    change slightly

--- mysite/useful_funcs.py
+++ mysite/useful_funcs.py
@@ -8,4 +8,4 @@ def print_colored_text(code, *styles):
     for style in styles:
         text_to_print += style
     text_to_print += str(code)
-    print(f"{text_to_print}" + Fore.RESET, Back.RESET, Style.RESET_ALL)
+    print(f"{text_to_print}{Fore.RESET}{Back.RESET}{Style.RESET_ALL}")
