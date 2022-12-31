460ec23ef9d47fdc04070e1e14178a66c3fe58cc
kent8192 <kento.kiku@outlook.jp>
2022-12-31 18:04:07 +0900

    add 2 urls

--- tweets/urls.py
+++ tweets/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("create/", views.TweetCreateView.as_view(), name="create"),
     path("<int:pk>/detail", views.TweetDetailView.as_view(), name="detail"),
     path("<int:pk>/delete/", views.TweetDeleteView.as_view(), name="delete"),
-    # path('<int:pk>/like/', views.LikeView, name='like'),
-    # path('<int:pk>/unlike/', views.UnlikeView, name='unlike'),
+    path("<int:pk>/like/", views.LikeView.as_view(), name="like"),
+    path("<int:pk>/unlike/", views.UnlikeView.as_view(), name="unlike"),
 ]
