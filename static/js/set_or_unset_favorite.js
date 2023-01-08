b08d0e8d69e83f9958e96091f3c4dc3c88abe5c2
kent8192 <kento.kiku@outlook.jp>
2023-01-08 15:09:22 +0900

    impl set_or_unset_favorite_tweet()

--- static/js/set_or_unset_favorite.js
+++ static/js/set_or_unset_favorite.js
@@ -1,47 +1,46 @@
-function set_favorite_tweet(tweet_url) {
-    // Fetch APIの実行
-    fetch('api.json', {
-        headers: {
-            'Content-Type': `${tweet_url}; charset=utf-8`
-        },
-        body: JSON.stringify()
-    })
-        // 通信が成功したとき
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (json) {
-            console.log("set success!");
-            console.log(`url: ${tweet_url}`);
-            console.log(json);
-        })
-        // 通信が失敗したとき
-        .catch(function (error) {
-            console.error(`set error:${error} `);
-            console.error(`url: ${tweet_url}`);
-        });
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
 
-function unset_favorite_tweet(tweet_url) {
-    // Fetch APIの実行
-    fetch('api.json', {
+function set_or_unset_favorite_tweet(tweet_url) {
+    const csrftoken = getCookie('csrftoken');
+    const request_options = {
+        'method': 'POST',
         headers: {
-            'Content-Type': `${tweet_url}; charset = utf - 8`
-        },
-        body: JSON.stringify()
+            'X-CSRFToken': csrftoken
+        }
+    };
+    const request = new Request(tweet_url, request_options);
+    // Fetch APIの実行
+    fetch(tweet_url, {
+        method: 'POST',
+        headers: {
+            'X-CSRFToken': csrftoken
+        }
     })
         // 通信が成功したとき
-        .then(function (response) {
-            return response.json();
+        .then((response) => {
+            console.log(response);
         })
-        .then(function (json) {
-            console.log("unset success!");
-            console.log(`url: ${tweet_url}`);
+        .then((json) => {
+            window.location.reload();
+            console.log(`done successfully! at url: ${tweet_url}`);
             console.log(json);
         })
         // 通信が失敗したとき
         .catch(function (error) {
-            console.error(`error:${error} `);
-            console.error(`url: ${tweet_url}`);
+            console.error(`error occured!: ${error} at url: ${tweet_url} `);
         });
 }
