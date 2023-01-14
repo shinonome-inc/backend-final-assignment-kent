b0b04de3e602845a23aed5e9db9811021357082e
kent8192 <kento.kiku@outlook.jp>
2023-01-14 16:41:42 +0900

    impl func mentioned by comment 1242343268

--- static/js/set_or_unset_favorite.js
+++ static/js/set_or_unset_favorite.js
@@ -16,16 +16,12 @@ function getCookie(name) {
 	return cookieValue;
 }
 
-function set_favorite_button_space_inner_html(tweet_id,is_favorite) {
-    alert("Hello World");
-    let favorite_button = document.getElementById("favorite_button" + tweet_id);
-    let unfavorite_button = document.getElementById("unfavorite_button" + tweet_id);
-    let url = is_favorite ? `{% url 'tweets:unfavorite' ${tweet_id} %}')` : `{% url 'tweets:unfavorite' ${tweet_id} %}')`;
-    favorite_button.style.visibility = is_favorite ? "hidden" : "visible";
-    unfavorite_button.style.visibility = is_favorite ? "visible" : "hidden";
+function set_or_unset_favorite_tweet(event) {
+	set_or_unset_favorite_tweet_backend_process(event)
 }
 
-function set_or_unset_favorite_tweet(tweet_url,tweet_id,is_favorite) {
+function set_or_unset_favorite_tweet_backend_process(click_event) {
+	const tweet_url = click_event.target.dataset.tweetUrl;
 	const csrftoken = getCookie("csrftoken");
 	const request_options = {
 		method: "POST",
@@ -34,23 +30,25 @@ function set_or_unset_favorite_tweet(tweet_url,tweet_id,is_favorite) {
 		},
 	};
 	// Fetch APIの実行
-	fetch(tweet_url, {
-		method: "POST",
-		headers: {
-			"X-CSRFToken": csrftoken,
-		},
-	})
+	fetch(tweet_url, request_options)
 		// 通信が成功したとき
 		.then((response) => {
 			console.log(response);
 		})
 		.then((json) => {
-			console.log(`done successfully! at url: ${tweet_url}`);
-			set_favorite_button_space_inner_html(tweet_id,is_favorite);
+			console.log(`done successfully! @ url: ${tweet_url}`);
+			set_or_unset_favorite_tweet_frontend_process(click_event);
 			console.log(json);
 		})
 		// 通信が失敗したとき
 		.catch(function (error) {
-			console.error(`error occured!: ${error} at url: ${tweet_url} `);
+			console.error(`error occured!: ${error} @ url: ${tweet_url} `);
 		});
+}
+function set_or_unset_favorite_tweet_frontend_process(event) {
+	const tweet_id = event.target.dataset.tweetId;
+	const favorite_button = document.getElementById("favorite_button" + tweet_id);
+	const unfavorite_button = document.getElementById("unfavorite_button" + tweet_id);
+	favorite_button.style.visibility = favorite_button.style.visibility == "hidden" ? "visible" : "hidden";
+	unfavorite_button.style.visibility = favorite_button.style.visibility == "hidden" ? "visible" : "hidden";
 }
